58f68a804351f1577830dc475f2d52cd9271cad5
minhaj-shakeel <cs1160354@iitd.ac.in>
2018-09-15 02:07:44 +0530

    added neighbour function

--- player.h
+++ player.h
@@ -4,18 +4,17 @@
 #include <board.h>
 #include <vector>
 #include <utility>
-#define point pair<int,int> 
+#define pii pair<int,int> 
 using namespace std;
-struct pair
-{
-	int x , y ;
 
-};
 struct move
 {
-	point start ;
-	point end ;
+	pii start ;
+	pii end ;
 };	
+
+
+
 class player
 {
 	int id ; //by default player1
@@ -24,9 +23,14 @@ class player
 	//int rings_y[] = {0,1,1,0,-1,-1};
 	player(int a);
 
-	vector<move<point,point> > generate_neighbour(int id );
-	move<point,point> generate_move(int id , board b);
-	void player::execute_move(move<point,point> m)
+	vector<move<pii,pii> > generate_neighbour(int id );
+	move<pii,pii> generate_move(int id , board b);
+
+	string generate_ring_place(int id , board b); //generate 
+	string generate_ring_move(int id , board b) ;
+
+
+	void player::execute_move(move<pii,pii> m)
 
 
 };
